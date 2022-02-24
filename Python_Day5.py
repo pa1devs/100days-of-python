fc3af260a349ee26633233f50f09b120e3a6c9cd
Pa1 Dev <pa1krdev@gmail.com>
2022-02-24 16:37:20 +0530

    Create Python_Day5.py

---
+++ Python_Day5.py
@@ -0,0 +1,103 @@
+#Password Generator Project Pro Code
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level
+# password = ""
+
+# for char in range(1, nr_letters + 1):
+#   password += random.choice(letters)
+
+# for char in range(1, nr_symbols + 1):
+#   password += random.choice(symbols)
+
+# for char in range(1, nr_numbers + 1):
+#   password += random.choice(numbers)
+
+# print(password)
+
+#Hard Level
+password_list = []
+
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
+
+
+
+#My Version
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+#print(len.names))
+totalet = int(len(letters))
+for letter in range(0,nr_letters):
+  rand = random.randint(0, totalet-1)
+#print(type(total))
+  
+
+  lword = str(letters[rand])
+  wordp = print(lword,end='')
+
+totalsym = int(len(symbols))
+for letter in range(0,nr_symbols):
+  rand = random.randint(0, totalsym-1)
+#print(type(total))
+  
+
+  symb = str(symbols[rand])
+  symp = print(symb,end='')
+
+totalnum = int(len(numbers))
+for letter in range(0,nr_numbers):
+  rand = random.randint(0, totalnum-1)
+#print(type(total))
+  
+
+  num = str(numbers[rand])
+  nump = print(num,end='')
+
+# password = wordp.append()+nump.append()
+# print(password)
+#print("wordp"+"symp"+"nump")
+#passw = 0
+# passlen = int(nr_letters+nr_symbols+nr_numbers)
+# for passd in range(0,passlen):
+# print(f"Your Strong System Generated Password is: {wordp}       {symp}{nump} ")
