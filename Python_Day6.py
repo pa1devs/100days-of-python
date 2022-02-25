ed53a3cf2fc63a6b92bb440c54bb287867e32393
Pa1 Dev <99909520+pa1devs@users.noreply.github.com>
2022-02-25 23:59:20 +0530

    Add files via upload

---
+++ Python_Day6.py
@@ -0,0 +1,23 @@
+#Day6_Final
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+hurdles = 6
+while hurdles > 0:
+    jump()
+    if at_goal()== True:
+        break
+    hurdles -= 1
+    print(hurdles)
